1f8c79ed932909fcea3afa651dc8e921b0a5dbc8
JooMin Choi <joominchoi23@gmail.com>
2021-10-22 20:34:24 +0100

    made api call to fetch weather data for corresponding city location information

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,9 +1,64 @@
 import React from 'react'
+import cities from '../../lib/city.list.json'
 
-export default function City() {
+export async function getServerSideProps(context) {
+  const city = getCity(context.params.city);
+
+  if(!city) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&units=metric&exclude=minutely`
+  );
+
+  const data = await res.json();
+
+  if(!data) {
+    return {
+      notFound: true
+    }
+  }
+  
+  console.log(data);
+
+  const slug = context.params.city;
+
+  return {
+    props: {
+      slug: slug,
+    },
+  };
+}
+
+const getCity = param => {
+  const cityParam = param.trim();
+  //get the id of the city
+  const splitCity = cityParam.split("-")
+  const id = splitCity[splitCity.length - 1];
+  console.log(id);
+
+  if(!id) {
+    return null;
+  }
+
+  const city = cities.find(city => city.id.toString() == id);
+
+  if(city) {
+    return city;
+  } else {
+    return null
+  }
+};
+
+
+export default function City({slug}) {
   return (
     <div>
       <h1>City Page</h1>
+      <h2>{slug}</h2>
     </div>
   )
 }
