33e97bbd0c5c354aa04f1f66ab680fc713985601
JooMin Choi <joominchoi23@gmail.com>
2021-10-22 19:42:27 +0100

    added homepage & searchbox that outputs results of cities typed in with unique id

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head"
+import SearchBox from "../components/SearchBox";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Head>
+        <title>Weather App - Next.js</title>
+      </Head>
+
+      <div className="home">
+        <div className="container">
+          <SearchBox />
+        </div>
+      </div>
     </div>
   );
 }
